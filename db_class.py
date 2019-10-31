ab499ad63aae3bec7639919d1a469bf1293d9e1e
Egor Nikolskii <goga.nik1221@gmail.com>
2019-10-31 21:01:29 +0600

    merge 2 branches

---
+++ db_class.py
@@ -0,0 +1,104 @@
+import sqlite3
+
+
+class DB:    
+
+    def __init__(self):
+        """Constructor"""
+        pass
+    
+    def create_db(self):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute( """CREATE TABLE recepies 
+         (id integer PRIMARY KEY AUTOINCREMENT , name text, category text, recepie text, ingredients text)""")
+        except sqlite3.OperationalError: print('table recepies already exists')  
+
+        try:
+            cursor.execute("""CREATE TABLE clients
+         (id integer PRIMARY KEY AUTOINCREMENT , name text)""")
+        except sqlite3.OperationalError: print('table clients already exists')
+
+        try:
+            cursor.execute("""CREATE TABLE favourites
+            (id integer PRIMARY KEY AUTOINCREMENT , id_client integer, id_recepie integer, clients_recepie boolean,             
+            UNIQUE(id_client,id_recepie),
+            FOREIGN KEY (id_client) REFERENCES clients(id), FOREIGN KEY (id_recepie) REFERENCES recepies(id))""")
+        except sqlite3.OperationalError: print('table favourites already exists')  
+
+        connection.commit()
+        connection.close()
+
+    def query(self, sql: str):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+        try:
+            cursor.execute(sql)
+        except sqlite3.IntegrityError as err: print(err)     
+        ans = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return ans
+
+    def insert_many(self, table: list, values: list):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+        existing_tables = {'recepies' : 4,'clients' : 1 ,'favourites': 2}
+        tables_col = {'recepies' : "'name', 'category','recepie','ingredients'",'clients' : "name" ,'favourites': "'id_client', 'id_recepie'"} 
+        if table in existing_tables:
+            try:
+                query = f"INSERT INTO {table} ({tables_col[table]}) VALUES ({','.join('?' for i in range(existing_tables[table]))})"
+                cursor.executemany(query, values)
+            except sqlite3.IntegrityError as err: print(err)
+        connection.commit()
+        connection.close()
+
+    def get_random_recepie(self):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""SELECT name, category, recepie, ingredients FROM recepies ORDER BY RANDOM() LIMIT 1""")
+        except sqlite3.IntegrityError as err: print(err)     
+        ans = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return ans
+
+    def get_favourites(self, client: str):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+        try:
+            cursor.execute(f"SELECT name, category, recepie, ingredients FROM recepies WHERE id in \
+            (SELECT id_recepie FROM favourites WHERE id_client = \
+            (SELECT id FROM clients WHERE name = '{client}'))")
+        except sqlite3.IntegrityError as err: print(err)     
+        ans = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return ans
+
+    def get_recepie_by_name(self, rec_name: str):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+        try:
+            cursor.execute(f"SELECT name, category, recepie, ingredients FROM recepies WHERE name = '{rec_name}'")
+        except sqlite3.IntegrityError as err: print(err)     
+        ans = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return ans
+
+    def get_recepie_by_ingredients(self, ingredients_list: list):
+        connection = sqlite3.connect('cook_ass_db')
+        cursor = connection.cursor()
+        try:
+            cursor.execute(f"SELECT name, category, recepie, ingredients FROM recepies \
+                 WHERE ingredients like '%{'%'.join(ingredients_list)}%'")
+        except sqlite3.IntegrityError as err: print(err)     
+        ans = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return ans
+
--- handler.py
+++ handler.py
@@ -1,18 +1,24 @@
 import logging
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import ConversationHandler
+from db_class import * 
+
+cs_db = DB()
+cs_db.create_db()
 
 # Стартовое окно с клавиатурой
 def welc_user(bot,update):
     text = 'Welcome'
     logging.info(text)
     cook_key = ReplyKeyboardMarkup([['Получить случайный рецепт','Добавить свой рецепт'],
+                                    ['Найти рецепт по названию','Найти рецепт по ингредиентам'],
                                     ['Избранное', 'Справка']])
     update.message.reply_text(text,reply_markup = cook_key)
 
 # Функция для Handler получить случайный рецепт
 def get_recipe(bot,update):
-    text_rec = 'РЕЦЕПТ'
+    res = cs_db.get_random_recepie()
+    text_rec = f'Название: {res[0]} \nКатегория:{res[1]} \nИнгридиенты: {res[2]} \nРецепт: {res[3]}'    
     update.message.reply_text(text_rec)
 
 # Функция для Handler справка
@@ -49,3 +55,24 @@ def own_recipe_skip(bot,update):
     text_skip = 'конец ввода'
     update.message.reply_text(text_skip)
     return ConversationHandler.END   
+
+# Функция поиска рецепта по названию
+def get_rec_by_name(bot,update):
+    update.message.reply_text("Введите название блюда: "
+    ,reply_markup=ReplyKeyboardRemove())
+    name = update.message.text
+    ans = cs_db.get_recepie_by_name(name)
+    text_rec = []
+    for i in ans:
+        text_rec.append(f'Название: {i[0]} \nКатегория:{i[1]} \nИнгридиенты: {i[2]} \nРецепт: {i[3]}')
+    text_rec = '\n\n'.join(text_rec)
+    update.message.reply_text(text_rec)
+
+#TODO Функция поиска рецепта по ингредиентам
+""" def get_rec_by_ingr(bot,update):
+    update.message.reply_text("Введите ингридиенты: "
+    ,reply_markup=ReplyKeyboardRemove())
+    ingredients = update.message.text
+    res = cs_db.get_recepie_by_ingredients(ingredients)
+    text_rec = f'Название: {res[0]} \nКатегория:{res[1]} \nИнгридиенты: {res[2]} \nРецепт: {res[3]}'
+    update.message.reply_text(text_rec) """
