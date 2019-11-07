5afcd63a190b61c27a77abf4055b9eae5562a6fa
Paradiss <esasha-05@yandex.ru>
2019-11-07 21:01:25 +0300

    Add files via upload
    пример теперь подробнее

--- sample.py
+++ sample.py
@@ -10,5 +10,7 @@ cs_db.add_to_favourites(479236808, '"зубы вампира" из яблок и
 cs_db.add_to_favourites(777, '"зубы вампира" из яблок и конфет')
 print(cs_db.get_favourites(479236808))
 
-print(cs_db.get_recepie_by_ingredients(['Сметана','Майонез']))
-print(cs_db.get_recepie_by_ingredients(['Майонез','Сметана']))
+ingr_list1 = ['Сметана','Майонез']
+ingr_list2 = ['Майонез','Сметана']
+print(cs_db.get_recepie_by_ingredients(ingr_list1))
+print(cs_db.get_recepie_by_ingredients(ingr_list2))
